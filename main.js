ffb886cb7746d843a197492883fd873b355326b4
vicky lu <vicky.lu@IC-c02fq6m5md6r.attlocal.net>
2021-10-24 13:16:23 -0400

    function on dragging multiple divs

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // elements
-var selectedEl = document.querySelector('div');
+var selectedEl;
 // var mouseDown = false;
 
 //add eventLiistener
@@ -11,9 +11,17 @@ var moveEvent = function(event){
     })
 }
 
-selectedEl.addEventListener('mousedown',function(){
-     document.addEventListener('mousemove',moveEvent);
-});
+
+var divs = document.querySelectorAll('div');
+console.log(divs);
+
+for (var i=0; i<divs.length;i++){
+    // console.log(divs[div]);
+    divs[i].addEventListener('mousedown',function(e){
+        selectedEl = e.target;
+        document.addEventListener('mousemove',moveEvent);
+   });
+}
 
 
 // add eventListener 
