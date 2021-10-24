6483c27e580681ed71c7a0c5eac1bab059ac55ba
vicky lu <vicky.lu@IC-c02fq6m5md6r.attlocal.net>
2021-10-24 19:40:26 -0400

    with offset

--- main.js
+++ main.js
@@ -1,24 +1,40 @@
 // elements
 var selectedEl;
+var offsetx;
+var offsety
 // var mouseDown = false;
 
 //add eventLiistener
 var moveEvent = function(event){
-    selectedEl.style.left = event.clientX+'px';
-    selectedEl.style.top = event.clientY+'px';
+
+    selectedEl.style.left = event.clientX - offsetx +'px';
+    selectedEl.style.top = event.clientY - offsety +'px';
     document.addEventListener('mouseup',function(){
         document.removeEventListener('mousemove', moveEvent);
     })
 }
 
 
-var divs = document.querySelectorAll('div');
-console.log(divs);
+var divs = document.querySelectorAll('.draggable');
+// console.log(divs);
 
-for (var i=0; i<divs.length;i++){
+for (var i=0; i<divs.length; i++){
     // console.log(divs[div]);
     divs[i].addEventListener('mousedown',function(e){
-        selectedEl = e.target;
+        selectedEl = e.currentTarget;
+
+        // calculate offset - x
+        elx = parseInt(selectedEl.getBoundingClientRect().left);
+        mousex = parseInt(e.clientX);
+        offsetx = mousex - elx;
+        console.log("x:", elx, mousex, offsetx);
+
+        // calculate offset - y
+        ely = parseInt(selectedEl.getBoundingClientRect().top);
+        mousey = parseInt(e.clientY);
+        offsety = mousey - ely;
+        console.log('y:', ely, mousey, offsety);
+
         document.addEventListener('mousemove',moveEvent);
    });
 }
